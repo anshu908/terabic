8fb36dfd4e86a5f5f9565459987e891d8cdbbed9
MisteryDevs <officialbotdev@gmail.com>
2025-04-19 10:41:39 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -144,7 +144,7 @@ def format_size(size):
 
 def shorten_url(url):
     #You can change api_url with your choice shortener
-    api_url = "https://api.modijiurl.com/api"
+    api_url = " https://api.gplinks.com/api?api="
     params = {
         "api": SHORTENER_API,
         "url": url
